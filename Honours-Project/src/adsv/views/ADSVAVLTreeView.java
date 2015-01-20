a7e85aafdcc4fd8f97d139d244a42b78dce848e5
Chimwemwe <ck.1@hotmail.co.uk>
2015-01-20 12:19:16 +0000

    Basic intertsion working

--- Honours-Project/src/adsv/views/ADSVAVLTreeView.java
+++ Honours-Project/src/adsv/views/ADSVAVLTreeView.java
@@ -3,6 +3,7 @@ package adsv.views;
 import adsv.avlTreeModel.AVLVertex;
 import adsv.globals.Constants;
 import adsv.globals.GenericFunctions;
+import edu.usfca.ds.shapes.DSShapeLink;
 import edu.usfca.xj.appkit.gview.object.GElement;
 import edu.usfca.xj.appkit.gview.object.GLink;
 
@@ -13,7 +14,8 @@ public class ADSVAVLTreeView extends DSView {
     private static final int FIND = 3;
 
     // The width/height difference between two levels in the tree for insertion
-    private static final int WIDTH_DELTA = 20;
+    //private static final int WIDTH_DELTA = 20;
+    private static final int WIDTH_DELTA = 50;
     private static final int HEIGHT_DELTA = 50;
 
     // The x and y coordination of the root
@@ -22,7 +24,7 @@ public class ADSVAVLTreeView extends DSView {
 
     //
     private static final int INSERTION_X_POSITION = 100;
-    private static final int INSERTION_Y_POSITION = 100;
+    private static final int INSERTION_Y_POSITION = 50;
 
     //Length for a sqaure leaf vertex
     private static final int LEAF_LENGTH = 20;
@@ -75,29 +77,62 @@ public class ADSVAVLTreeView extends DSView {
         if (root == null) {
             root = insertRootVertex(vertexToInsert);
         } else {
-            AVLVertex newVertex = createVertexAt(vertexToInsert, INSERTION_X_POSITION, INSERTION_Y_POSITION);
-            repaintwait();
-            AVLVertex vertexBeingReplaced = findInsertionLocation(vertexToInsert);
+            insertIntoTree(vertexToInsert);
         }
     }
 
     private AVLVertex insertRootVertex(int vertexToInsert) {
-        return createVertexAt(vertexToInsert, ROOT_X_POSITION, ROOT_Y_POSITION);
-    }
-
-    private void insertIntoTree(int vertexToInsert) {
-        AVLVertex vertexBeingReplaced = findInsertionLocation(vertexToInsert);
-    }
-
-    private AVLVertex createVertexAt(int vertexToInsert, int vertexXPosition, int vertexYPosition) {
-        GElement graphicalRoot = createCircle(String.valueOf(vertexToInsert), vertexXPosition, vertexYPosition);
-        AVLVertex parentVertex = AVLVertex.newVertex(vertexToInsert, graphicalRoot);
-        addLeafChildren(parentVertex, vertexXPosition, vertexYPosition);
+        AVLVertex parentVertex = createVertexAt(vertexToInsert, ROOT_X_POSITION, ROOT_Y_POSITION);
+        addLeafChildren(parentVertex);
 
         return parentVertex;
     }
 
-    private void addLeafChildren(AVLVertex parentVertex, int parentXPosition, int parentYPosition) {
+    private void insertIntoTree(int vertexToInsert) {
+        AVLVertex newVertex = createVertexAt(vertexToInsert, INSERTION_X_POSITION, INSERTION_Y_POSITION);
+        repaintwait();
+
+        AVLVertex leafVertexToReplace = findInsertionLocation(vertexToInsert);
+        replaceLeafVertex(newVertex, leafVertexToReplace);
+        addLeafChildren(newVertex);
+        removeAny(highlightCircle);
+    }
+
+    private void replaceLeafVertex(AVLVertex newVertex, AVLVertex leafVertexToReplace) {
+        positionVertex(newVertex, leafVertexToReplace);
+        AVLVertex parent = leafVertexToReplace.parent;
+
+        // Replace the leaf vertex with the new vertex
+        // by adopting new vertex as child
+        if (leafVertexToReplace.isLeftChild()) {
+            addLeftChild(parent, newVertex);
+        } else {
+            addRightChild(parent, newVertex);
+        }
+
+        removeLink(parent.graphicVertex, leafVertexToReplace.graphicVertex);
+        removeAny(leafVertexToReplace.graphicVertex);
+    }
+
+
+    private void positionVertex(AVLVertex newVertex, AVLVertex leafVertexToReplace) {
+        GElement newGraphicVertex = newVertex.graphicVertex;
+        GElement leafGraphicVertex = leafVertexToReplace.graphicVertex;
+
+        AnimatePath(newGraphicVertex, newGraphicVertex.getPosition(), leafGraphicVertex.getPosition(), 40);
+    }
+
+    private AVLVertex createVertexAt(int vertexToInsert, int vertexXPosition, int vertexYPosition) {
+        GElement graphicVertex = createCircle(String.valueOf(vertexToInsert), vertexXPosition, vertexYPosition);
+        AVLVertex parentVertex = AVLVertex.newVertex(vertexToInsert, graphicVertex);
+
+        return parentVertex;
+    }
+
+    private void addLeafChildren(AVLVertex parentVertex) {
+        int parentXPosition = (int) parentVertex.graphicVertex.getPositionX();
+        int parentYPosition = (int) parentVertex.graphicVertex.getPositionY();
+
         GElement graphicalLeftLeaf = createRectangle("", childXPosition(parentXPosition, -1), childYPosition(parentYPosition), LEAF_LENGTH, LEAF_LENGTH);
         GElement graphicalRightLeaf = createRectangle("", childXPosition(parentXPosition, 1), childYPosition(parentYPosition), LEAF_LENGTH, LEAF_LENGTH);
         AVLVertex leftLeafVertex = AVLVertex.newLeafVertex(graphicalLeftLeaf);
@@ -110,13 +145,27 @@ public class ADSVAVLTreeView extends DSView {
     private void addLeftChild(AVLVertex parent, AVLVertex leftChild) {
         parent.leftChild = leftChild;
         leftChild.parent = parent;
-        createLink(parent.graphicalVertex, leftChild.graphicalVertex, GLink.SHAPE_ARC, GElement.ANCHOR_CENTER, GElement.ANCHOR_TOP, "", 1);
+        addGraphicEdge(parent, leftChild);
     }
 
     private void addRightChild(AVLVertex parent, AVLVertex rightChild) {
         parent.rightChild = rightChild;
         rightChild.parent = parent;
-        createLink(parent.graphicalVertex, rightChild.graphicalVertex, GLink.SHAPE_ARC, GElement.ANCHOR_CENTER, GElement.ANCHOR_TOP, "", 1);
+        addGraphicEdge(parent, rightChild);
+    }
+
+    private void addGraphicEdge(AVLVertex parent, AVLVertex child) {
+        DSShapeLink edge;
+
+        //If the child is a leaf vertex , edge should be anchored to the top of square
+        if (child.isLeafVertex()) {
+            edge = createLink(parent.graphicVertex, child.graphicVertex, GLink.SHAPE_ARC, GElement.ANCHOR_CENTER, GElement.ANCHOR_TOP, "", 1);
+        } else {
+            // Otherwise anchor the edge centrally
+            edge = createLink(parent.graphicVertex, child.graphicVertex, GLink.SHAPE_ARC, GElement.ANCHOR_CENTER, GElement.ANCHOR_CENTER, "", 1);
+        }
+
+        edge.setArrowVisible(false);
     }
 
     private int childXPosition(int parentXPosition, int offsetSign) {
@@ -192,18 +241,17 @@ public class ADSVAVLTreeView extends DSView {
                 }
                 animateVertexVisit(u);
             }
-            removeAny(highlightCircle);
             return u;
         }
     }
 
     private void animateVertexVisit(AVLVertex u) {
-        AnimatePath(highlightCircle, highlightCircle.getPosition(), u.graphicalVertex.getPosition(), 40);
+        AnimatePath(highlightCircle, highlightCircle.getPosition(), u.graphicVertex.getPosition(), 40);
         repaintwait();
     }
 
     private void setHighlightCircleAtRoot() {
-        highlightCircle = createCircle("", root.graphicalVertex.getPositionX(), root.graphicalVertex.getPositionY());
+        highlightCircle = createCircle("", root.graphicVertex.getPositionX(), root.graphicVertex.getPositionY());
         highlightCircle.setOutlineColor(Constants.ANDROID_RED);
         repaintwait();
     }
