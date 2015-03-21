c1e288273d9287ae3cfeda6d3ed75724082b065a
Chimwemwe <ck.1@hotmail.co.uk>
2015-03-21 15:03:12 +0000

    Tailor bubble sort animation speeds

--- Honours-Project/src/adv/panels/BubbleSortPanel.java
+++ Honours-Project/src/adv/panels/BubbleSortPanel.java
@@ -7,10 +7,22 @@ import adv.views.BubbleSortView;
 
 public class BubbleSortPanel extends SortPanel {
 
+	protected final static int SLOW_VALUE = 75;
+	protected final static int MEDIUM_VALUE = 50;
+	protected final static int FAST_VALUE = 15;
+	protected final static int VERY_FAST_VALUE = 5;
+
 	public BubbleSortPanel(Window window) {
 		super(window);
 		this.add(view = sortView = new BubbleSortView(), BorderLayout.CENTER);
 		setUpAnimationPanel(view);
+	}
+
+	protected void setAnimationSpeeds() {
+		slowValue = 200;
+		mediumValue = 150;
+		fastValue = 75;
+		veryFastValue = 15;
 	}
 	
 }
